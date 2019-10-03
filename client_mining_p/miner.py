6f2850b9780bc7827d0cecaf089296bce1a8ef9e
Matt Klein <mklein246@gmail.com>
2019-10-03 13:31:18 -0700

    first and second day work

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -1,10 +1,33 @@
 import hashlib
 import requests
+import json
 
 import sys
 
 
-# TODO: Implement functionality to search for a proof 
+def valid_proof(block_string, proof):
+    """
+    Validates the Proof:  Does hash(block_string, proof) contain 6
+    leading zeroes?  Return true if the proof is valid
+    :param block_string: <string> The stringified block to use to
+    check in combination with `proof`
+    :param proof: <int?> The value that when combined with the
+    stringified previous block results in a hash that has the
+    correct number of leading zeroes.
+    :return: True if the resulting hash is a valid proof, False otherwise
+    """
+    guess = f'{block_string}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+
+    return guess_hash[:6] == '000000'
+
+def proof_of_work(block):
+    block_string = json.dumps(block, sort_keys=True).encode()
+    proof = 0
+    while valid_proof(block_string, proof) is False:
+        proof += 1
+    
+    return proof
 
 
 if __name__ == '__main__':
@@ -17,11 +40,14 @@ if __name__ == '__main__':
     coins_mined = 0
     # Run forever until interrupted
     while True:
-        # TODO: Get the last proof from the server and look for a new one
-        # TODO: When found, POST it to the server {"proof": new_proof}
-        # TODO: We're going to have to research how to do a POST in Python
-        # HINT: Research `requests` and remember we're sending our data as JSON
-        # TODO: If the server responds with 'New Block Forged'
-        # add 1 to the number of coins mined and print it.  Otherwise,
-        # print the message from the server.
-        pass
+        response = requests.get(url=f'{node}/last_block')
+        last_block = response.json()['last_block']
+        proof = proof_of_work(last_block)
+        
+        response = requests.post(url=f'{node}/mine', json={"proof": proof})
+
+        if response.json()["message"] == 'New Block Forged':
+            coins_mined += 1
+            print(coins_mined)
+        else:
+            print(response.json()["message"])
--- credit_for_mining_p/miner.py
+++ credit_for_mining_p/miner.py
@@ -1,2 +1,66 @@
-# Paste your version of miner.py from the communication_gp
-# or client_mining_p folder here (we don't make any changes)
+import hashlib
+import requests
+import json
+import shelve
+import uuid
+
+import sys
+
+
+def valid_proof(block_string, proof):
+    """
+    Validates the Proof:  Does hash(block_string, proof) contain 6
+    leading zeroes?  Return true if the proof is valid
+    :param block_string: <string> The stringified block to use to
+    check in combination with `proof`
+    :param proof: <int?> The value that when combined with the
+    stringified previous block results in a hash that has the
+    correct number of leading zeroes.
+    :return: True if the resulting hash is a valid proof, False otherwise
+    """
+    guess = f'{block_string}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+
+    return guess_hash[:6] == '000000'
+
+def proof_of_work(block):
+    block_string = json.dumps(block, sort_keys=True).encode()
+    proof = 0
+    while valid_proof(block_string, proof) is False:
+        proof += 1
+    
+    return proof
+
+
+if __name__ == '__main__':
+    # get id
+    my_id = shelve.open('my_id')
+    if not "my_uuid" in my_id:
+        generated_id = str(uuid.uuid4())
+        table = str.maketrans(dict.fromkeys('-'))
+        my_id["my_uuid"] = generated_id.translate(table)
+    my_uuid = my_id["my_uuid"]
+    my_id.close()
+
+    # What node are we interacting with?
+    if len(sys.argv) > 1:
+        node = sys.argv[1]
+    else:
+        node = "http://localhost:5000"
+
+    # set counter to 0
+    coins_mined = 0
+
+    # Run forever until interrupted
+    while True:
+        response = requests.get(url=f'{node}/last_block')
+        last_block = response.json()['last_block']
+        proof = proof_of_work(last_block)
+        
+        response = requests.post(url=f'{node}/mine', json={"id": my_uuid, "proof": proof})
+
+        if response.json()["message"] == 'New Block Forged':
+            coins_mined += 1
+            print(coins_mined)
+        else:
+            print(response.json()["message"])
